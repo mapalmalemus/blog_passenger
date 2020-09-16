da9f8901724a942481b6919ae66e0c1c6e6d60f0
Miguel Palma <mapalmalemus@gmail.com>
2020-09-16 03:00:04 -0500

    Security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "mapalmalemus", password: "miwebexpreso", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
